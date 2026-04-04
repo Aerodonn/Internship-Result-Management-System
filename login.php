c8e5f8e0c7e500f93e7e9553dcf6649c0e824b69
Min Pyae Phyo <subtominthebro@gmail.com>
2026-04-04 13:31:09 +0800

    added login frontend design along with relevent files to it

---
+++ login.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang='en'>
+<head>
+    <meta charset='utf-8'>
+    <meta name='viewport' content='width=device-width, initial-scale=1'>
+    <title>Page Title</title>
+    <link rel='stylesheet' href='login.css'>
+</head>
+<body>
+        
+    
+    <section>
+        <article class="formBox">
+            <form method="post" class="login">
+                <img src="assets\nottinghamLogo.png">
+                <h2>Intership Portal</h2>
+                <section id="dataRetrival">
+                    
+                    <label for="username">Username:</label><br>
+                    <input type="text" id="username" name="username" class="loginInput" ><br><br>
+                    
+                    <label for="password">Password:</label><br>
+                    <input type="password" id="password" name="password" class="loginInput"><br>
+                    <a id="forgotPass" href="#">forgot password?</a><br>
+                </section>
+
+                <section id="submitANDreset">
+                    <input type="submit" value="Submit Application" class="loginDone">
+                </section>
+            </form>
+        </article>
+    </section>
+
+    <footer>
+
+    </footer>
+</body>
+</html>
