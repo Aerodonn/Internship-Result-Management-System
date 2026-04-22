39dd49db5aeecb5e335e002b5b3904f97e8af138
Aerodonn <ooiciaran@gmail.com>
2026-04-22 20:29:28 +0800

    changed the sql skeleton; switched to snake_case

--- internship_management_system.sql
+++ internship_management_system.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: Apr 18, 2026 at 06:33 PM
+-- Generation Time: Apr 22, 2026 at 12:28 PM
 -- Server version: 5.7.24
 -- PHP Version: 8.3.1
 
@@ -28,12 +28,12 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `assessor` (
-  `UserID` int(11) NOT NULL,
-  `FullName` varchar(255) NOT NULL,
-  `PhoneNumber` varchar(22) DEFAULT NULL,
-  `EmailAddress` varchar(128) NOT NULL,
-  `Organisation` varchar(128) DEFAULT NULL,
-  `AssessorType` enum('Lecturer','Industry Supervisor') NOT NULL
+  `user_id` int(11) NOT NULL,
+  `full_name` varchar(255) NOT NULL,
+  `phone_number` varchar(22) DEFAULT NULL,
+  `email_address` varchar(128) NOT NULL,
+  `organisation` varchar(128) DEFAULT NULL,
+  `assessor_type` enum('Lecturer','Industry Supervisor') NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
@@ -43,29 +43,31 @@ CREATE TABLE `assessor` (
 --
 
 CREATE TABLE `internship` (
-  `InternID` int(11) NOT NULL,
-  `SupervisorID` int(11) NOT NULL,
-  `LecturerID` int(11) NOT NULL,
-  `StudentID` int(11) NOT NULL,
-  `InternshipCompany` varchar(255) DEFAULT NULL,
-  `StartDate` date DEFAULT NULL,
-  `EndDate` date DEFAULT NULL,
-  `ReportStatus` enum('Drafting','In Progress','Suspended','Finalisation','Complete') DEFAULT NULL
+  `intern_id` int(11) NOT NULL,
+  `supervisor_id` int(11) NOT NULL,
+  `lecturer_id` int(11) NOT NULL,
+  `student_id` int(11) NOT NULL,
+  `internship_company` varchar(255) DEFAULT NULL,
+  `start_date` date DEFAULT NULL,
+  `end_date` date DEFAULT NULL,
+  `report_status` enum('Drafting','In Progress','Suspended','Finalisation','Complete') DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `internshipreport`
+-- Table structure for table `internship_report`
 --
 
-CREATE TABLE `internshipreport` (
-  `InternID` int(11) NOT NULL,
-  `taskScore` decimal(5,2) DEFAULT '0.00',
-  `safetyScore` decimal(5,2) DEFAULT '0.00',
-  `theoryScore` decimal(5,2) DEFAULT '0.00',
-  `presentScore` decimal(5,2) DEFAULT '0.00',
-  `clarityScore` decimal(5,2) DEFAULT '0.00',
+CREATE TABLE `internship_report` (
+  `report_id` int(11) NOT NULL,
+  `intern_id` int(11) NOT NULL,
+  `assessor_id` int(11) NOT NULL,
+  `task_score` decimal(5,2) DEFAULT '0.00',
+  `safety_score` decimal(5,2) DEFAULT '0.00',
+  `theory_score` decimal(5,2) DEFAULT '0.00',
+  `present_score` decimal(5,2) DEFAULT '0.00',
+  `clarity_score` decimal(5,2) DEFAULT '0.00',
   `proj_mgmt_score` decimal(5,2) DEFAULT '0.00',
   `time_mgmt_score` decimal(5,2) DEFAULT '0.00',
   `comment` text
@@ -78,26 +80,26 @@ CREATE TABLE `internshipreport` (
 --
 
 CREATE TABLE `student` (
-  `StudentID` int(11) NOT NULL,
-  `StudentRegNumber` varchar(20) NOT NULL,
-  `StudentName` varchar(255) NOT NULL,
-  `EmailAddress` varchar(255) NOT NULL,
-  `Programme` varchar(255) DEFAULT NULL,
-  `EnrollmentDate` date DEFAULT NULL,
-  `AccountStatus` enum('Active','Graduated','On-leave','Suspended') DEFAULT 'Active'
+  `student_id` int(11) NOT NULL,
+  `student_reg_number` varchar(20) NOT NULL,
+  `student_name` varchar(255) NOT NULL,
+  `email_address` varchar(255) NOT NULL,
+  `programme` varchar(255) DEFAULT NULL,
+  `enrollment_date` date DEFAULT NULL,
+  `account_status` enum('Active','Graduated','On-leave','Suspended') DEFAULT 'Active'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `userlogin`
+-- Table structure for table `user_login`
 --
 
-CREATE TABLE `userlogin` (
-  `UserID` int(11) NOT NULL,
-  `Username` varchar(255) NOT NULL,
-  `Password` varchar(255) NOT NULL,
-  `SystemRole` enum('Admin','Assessor') NOT NULL
+CREATE TABLE `user_login` (
+  `user_id` int(11) NOT NULL,
+  `username` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `system_role` enum('Admin','Assessor') NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
@@ -108,37 +110,40 @@ CREATE TABLE `userlogin` (
 -- Indexes for table `assessor`
 --
 ALTER TABLE `assessor`
-  ADD PRIMARY KEY (`UserID`),
-  ADD UNIQUE KEY `EmailAddress` (`EmailAddress`);
+  ADD PRIMARY KEY (`user_id`),
+  ADD UNIQUE KEY `email_address` (`email_address`);
 
 --
 -- Indexes for table `internship`
 --
 ALTER TABLE `internship`
-  ADD PRIMARY KEY (`InternID`),
-  ADD KEY `SupervisorID` (`SupervisorID`),
-  ADD KEY `LecturerID` (`LecturerID`),
-  ADD KEY `StudentID` (`StudentID`);
+  ADD PRIMARY KEY (`intern_id`),
+  ADD KEY `supervisor_id` (`supervisor_id`),
+  ADD KEY `lecturer_id` (`lecturer_id`),
+  ADD KEY `student_id` (`student_id`);
 
 --
--- Indexes for table `internshipreport`
+-- Indexes for table `internship_report`
 --
-ALTER TABLE `internshipreport`
-  ADD PRIMARY KEY (`InternID`);
+ALTER TABLE `internship_report`
+  ADD PRIMARY KEY (`report_id`),
+  ADD UNIQUE KEY `intern_id` (`intern_id`),
+  ADD UNIQUE KEY `assessor_id` (`assessor_id`),
+  ADD UNIQUE KEY `intern_id_2` (`intern_id`,`assessor_id`);
 
 --
 -- Indexes for table `student`
 --
 ALTER TABLE `student`
-  ADD PRIMARY KEY (`StudentID`),
-  ADD UNIQUE KEY `StudentRegNumber` (`StudentRegNumber`),
-  ADD UNIQUE KEY `EmailAddress` (`EmailAddress`);
+  ADD PRIMARY KEY (`student_id`),
+  ADD UNIQUE KEY `student_reg_number` (`student_reg_number`),
+  ADD UNIQUE KEY `email_address` (`email_address`);
 
 --
--- Indexes for table `userlogin`
+-- Indexes for table `user_login`
 --
-ALTER TABLE `userlogin`
-  ADD PRIMARY KEY (`UserID`);
+ALTER TABLE `user_login`
+  ADD PRIMARY KEY (`user_id`);
 
 --
 -- AUTO_INCREMENT for dumped tables
@@ -148,19 +153,19 @@ ALTER TABLE `userlogin`
 -- AUTO_INCREMENT for table `internship`
 --
 ALTER TABLE `internship`
-  MODIFY `InternID` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `intern_id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `student`
 --
 ALTER TABLE `student`
-  MODIFY `StudentID` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `student_id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
--- AUTO_INCREMENT for table `userlogin`
+-- AUTO_INCREMENT for table `user_login`
 --
-ALTER TABLE `userlogin`
-  MODIFY `UserID` int(11) NOT NULL AUTO_INCREMENT;
+ALTER TABLE `user_login`
+  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- Constraints for dumped tables
@@ -170,21 +175,22 @@ ALTER TABLE `userlogin`
 -- Constraints for table `assessor`
 --
 ALTER TABLE `assessor`
-  ADD CONSTRAINT `assessor_ibfk_1` FOREIGN KEY (`UserID`) REFERENCES `userlogin` (`UserID`);
+  ADD CONSTRAINT `assessor_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `user_login` (`user_id`);
 
 --
 -- Constraints for table `internship`
 --
 ALTER TABLE `internship`
-  ADD CONSTRAINT `internship_ibfk_1` FOREIGN KEY (`SupervisorID`) REFERENCES `assessor` (`UserID`),
-  ADD CONSTRAINT `internship_ibfk_2` FOREIGN KEY (`LecturerID`) REFERENCES `assessor` (`UserID`),
-  ADD CONSTRAINT `internship_ibfk_3` FOREIGN KEY (`StudentID`) REFERENCES `student` (`StudentID`);
+  ADD CONSTRAINT `internship_ibfk_1` FOREIGN KEY (`supervisor_id`) REFERENCES `assessor` (`user_id`),
+  ADD CONSTRAINT `internship_ibfk_2` FOREIGN KEY (`lecturer_id`) REFERENCES `assessor` (`user_id`),
+  ADD CONSTRAINT `internship_ibfk_3` FOREIGN KEY (`student_id`) REFERENCES `student` (`student_id`);
 
 --
--- Constraints for table `internshipreport`
+-- Constraints for table `internship_report`
 --
-ALTER TABLE `internshipreport`
-  ADD CONSTRAINT `internshipreport_ibfk_1` FOREIGN KEY (`InternID`) REFERENCES `internship` (`InternID`);
+ALTER TABLE `internship_report`
+  ADD CONSTRAINT `internship_report_ibfk_1` FOREIGN KEY (`intern_id`) REFERENCES `internship` (`intern_id`),
+  ADD CONSTRAINT `internship_report_ibfk_2` FOREIGN KEY (`assessor_id`) REFERENCES `assessor` (`user_id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
